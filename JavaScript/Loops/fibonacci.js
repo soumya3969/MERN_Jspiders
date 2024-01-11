3efbb2b190b47c5511efa2fd4522182d6b3abdf3
soumya3969 <srmhnt2001@gmail.com>
2024-01-11 20:39:07 +0530

    fibonacci series

---
+++ JavaScript/Loops/fibonacci.js
@@ -0,0 +1,13 @@
+const readline = require("readline-sync");
+function fibonacci() {
+  let n = +readline.question("Enter Series Length:- ");
+  let a = 0;
+  let b = 1;
+  for (let i = 0; i <= n; i++) {
+    console.log(a);
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+}
+fibonacci();
