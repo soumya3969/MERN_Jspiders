e663560df46784d7f0d59a1c17fc7f5d79558696
soumya3969 <srmhnt2001@gmail.com>
2024-02-05 15:34:47 +0530

    emp dashboard

--- JavaScript/DOM/EmpDashBoard/script.js
+++ JavaScript/DOM/EmpDashBoard/script.js
@@ -1,83 +1,139 @@
-let employee=[
-    {
-        id:101,
-        name:'Rashmi',
-        gender:'Female',
-        degn:'jnr developer',
-        age:28,
-        salary: 22000,
-        company: Aloha,
-        city:'Bangalore',
-        state:'Karnataka'
-    },
-    {
-        id:102,
-        name:'Soumya',
-        gender:'Male',
-        degn:'Intern',
-        age:24,
-        salary: 16000,
-        company: 'Jspiders',
-        city:'Bhubaneswar',
-        state:'Odisha'
-    },
-    {
-        id:103,
-        name:'Sandhya',
-        gender:'Female',
-        degn:'Intern',
-        age:24,
-        salary:16000 ,
-        company: 'Jspiders',
-        city:'Raipur',
-        state:'CG'
-    },
-    {
-        id:104,
-        name:'Goutam',
-        gender:'Male',
-        degn:'Manager',
-        age:31,
-        salary: 45000,
-        company: 'orient',
-        city:'Bangalore',
-        state:'Karnataka'
-    },
-    {
-        id:105,
-        name:'Sumit',
-        gender:'Male',
-        degn:'Senior developer',
-        age:23,
-        salary: 30000,
-        company: 'flipkart',
-        city:'Bangalore',
-        state:'Karnataka'
-    }
+let employee = [
+  {
+    id: 101,
+    name: "Rashmi",
+    gender: "Female",
+    degn: "jnr developer",
+    age: 28,
+    salary: 22000,
+    company: "Aloha",
+    city: "Bangalore",
+    state: "Karnataka"
+  },
+  {
+    id: 102,
+    name: "Soumya",
+    gender: "Male",
+    degn: "Intern",
+    age: 24,
+    salary: 16000,
+    company: "Jspiders",
+    city: "Bhubaneswar",
+    state: "Odisha"
+  },
+  {
+    id: 103,
+    name: "Sandhya",
+    gender: "Female",
+    degn: "Intern",
+    age: 24,
+    salary: 16000,
+    company: "Jspiders",
+    city: "Raipur",
+    state: "CG"
+  },
+  {
+    id: 104,
+    name: "Goutam",
+    gender: "Male",
+    degn: "Manager",
+    age: 31,
+    salary: 45000,
+    company: "orient",
+    city: "Bangalore",
+    state: "Karnataka"
+  },
+  {
+    id: 105,
+    name: "Sumit",
+    gender: "Male",
+    degn: "Senior developer",
+    age: 23,
+    salary: 30000,
+    company: "flipkart",
+    city: "Bangalore",
+    state: "Karnataka"
+  }
 ];
-function displayEmp (emps) {
-    if (emps.length>0) {
-        let eachEmp=` `;
-        for (const emp of emps) {
-            eachEmp+=` <div class="col-12">
-            <div class="card">
-                <div class="card-body d-flex justify-content-evenly bg-gradient ">
-                   <h5>101</h5>
-                   <h5>Rashmi</h5>
-                   <h5>Female</h5>
-                   <h5>Jnr Developer</h5>
-                   <h5>28</h5>
-                   <h5>22000</h5>
-                   <h5>Aloha</h5>
-                   <h5>Bangalore</h5>
-                   <h5>Karnataka</h5>
-                   </div>
-                </div>
+// * TO DISPLAY ALL EMPLOYEE DETAILS
+function displayAllEmp(emps) {
+  if (emps.length > 0) {
+    let detailsHTML = "";
+    for (const emp of emps) {
+      detailsHTML += `
+        <div class="col-12" style='background: #e0ffcd75 !important;'>
+          <div class="card" >
+            <div class="card-body d-flex justify-content-evenly" >
+              <h5>${emp.id}</h5>
+              <h5>${emp.name}</h5>
+              <h5>${emp.gender}</h5>
+              <h5>${emp.degn}</h5>
+              <h5>${emp.age}</h5>
+              <h5>${emp.salary}</h5>
+              <h5>${emp.company}</h5>
+              <h5>${emp.city}</h5>
+              <h5>${emp.state}</h5>
             </div>
-        </div>`
-            
-        }
-        document.getElementById('details').innerHTML+=eachEmp;
+          </div>
+        </div>`;
     }
+
+    document.getElementById("details").innerHTML = detailsHTML;
+  }
 }
-displayEmp(employee)
+// displayAllEmp(employee);
+// * TO DISPLAY ONLY MALE EMPLOYEES
+function displayMaleEmp(emps) {
+  if (emps.length > 0) {
+    let detailsHTML = "";
+    for (const emp of emps) {
+      if (emp.gender == "Male") {
+        detailsHTML += `
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body d-flex justify-content-evenly bg-gradient ">
+                <h5>${emp.id}</h5>
+                <h5>${emp.name}</h5>
+                <h5>${emp.gender}</h5>
+                <h5>${emp.degn}</h5>
+                <h5>${emp.age}</h5>
+                <h5>${emp.salary}</h5>
+                <h5>${emp.company}</h5>
+                <h5>${emp.city}</h5>
+                <h5>${emp.state}</h5>
+              </div>
+            </div>
+          </div>`;
+      }
+    }
+    document.getElementById("details").innerHTML = detailsHTML;
+  }
+}
+
+// * TO DISPLAY ONLY FEMALE EMPLOYEES
+function displayFemaleEmp(emps) {
+  if (emps.length > 0) {
+    let detailsHTML = "";
+    for (const emp of emps) {
+      if (emp.gender == "Female") {
+        detailsHTML += `
+            <div class="col-12">
+              <div class="card">
+                <div class="card-body d-flex justify-content-evenly bg-gradient ">
+                  <h5>${emp.id}</h5>
+                  <h5>${emp.name}</h5>
+                  <h5>${emp.gender}</h5>
+                  <h5>${emp.degn}</h5>
+                  <h5>${emp.age}</h5>
+                  <h5>${emp.salary}</h5>
+                  <h5>${emp.company}</h5>
+                  <h5>${emp.city}</h5>
+                  <h5>${emp.state}</h5>
+                </div>
+              </div>
+            </div>`;
+      }
+    }
+    document.getElementById("details").innerHTML = detailsHTML;
+  }
+}
